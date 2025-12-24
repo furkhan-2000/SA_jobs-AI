9564967747c29545823f2fcf853fcda3177139b8
Khan Mohammed <kmohammed0200@gmail.com>
2025-12-24 23:03:33 +0300

    updated

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     JOOBLE_KEY: str = "bcf720ac-ffc5-429e-ae29-797dedf6ee44"
     CAREERJET_KEY: str = "6fde6cdcf3154cacfe7c6fe002c3c86c"
     OPENWEBNINJA_KEY: str = "ak_q7f0dwbhl7k7txtot1sp5ltun9lr7rwlgz2070mx1l3b53w"
-    DEBUG_MODE: bool = False # Added for debug toggle
+    DEBUG_MODE: bool = True
 
     class Config:
         env_file = ".env"
