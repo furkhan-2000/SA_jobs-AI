ba9eebcd0fdeaa0f07163af40417d91d151fcc76
Khan Mohammed <kmohammed0200@gmail.com>
2025-12-28 16:15:46 +0300

    updated

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_PORT: int = 7070
     DATABASE_URL: Optional[str] = None
-    LOG_LEVEL: str = "INFO"
+    LOG_LEVEL: str = "ERROR"
     MAX_RETRIES: int = 3
     TIMEOUT: int = 15
     PAGE_SIZE: int = 20
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     JOOBLE_KEY: str = "bcf720ac-ffc5-429e-ae29-797dedf6ee44"
     CAREERJET_KEY: str = "6fde6cdcf3154cacfe7c6fe002c3c86c"
     OPENWEBNINJA_KEY: str = "ak_q7f0dwbhl7k7txtot1sp5ltun9lr7rwlgz2070mx1l3b53w"
-    DEBUG_MODE: bool = True
+    DEBUG_MODE: bool = False
     AI_SERVICE_URL: Optional[str] = None  # TODO: Replace with the internal K8s DNS name for the AI microservice
 
     class Config:
